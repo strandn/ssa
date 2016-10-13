86c62055f0c1a8813be91d29dfd4d1524ad2f763
Alexander Prokic <gandhigeneric@yahoo.com>
2016-10-13 16:56:38 -0400

    Base API connectivity is in place

--- ssa/AppDelegate.swift
+++ ssa/AppDelegate.swift
@@ -12,10 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Connect to Grokker
+        Ugi.createSingleton()
+        Ugi.singleton().openConnection()
+        
         return true
     }
 
@@ -39,6 +43,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        // Close connection to Grokker
+        Ugi.singleton().closeConnection()
     }
 
 
--- ssa/ViewController.swift
+++ ssa/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UgiInventoryDelegate {
 
+    //MARK:Properties
+    @IBOutlet weak var displayTagLabel: UILabel!
+    //var tagToDetailString: [UgiTag : NSMutableString] = [:]
+    
+    func updateUI(){
+        let inventory: UgiInventory? = Ugi.singleton().activeInventory
+        if (inventory?.tags.count != 0) {
+            //let firstTag = Ugi.singleton().activeInventory.tags[0].epc.toString()
+            displayTagLabel.text = "Found \(inventory!.tags.count) Tag(s) Nearby"
+        }
+        else {
+            displayTagLabel.text = "No Tags Nearby"
+        }
+    }
+    
+    //MARK:Actions
+    @IBAction func readStartButton(_ sender: UIButton) {
+        Ugi.singleton().startInventory(
+            self,
+            with: UgiRfidConfiguration.config(withInventoryType: UgiInventoryTypes.UGI_INVENTORY_TYPE_LOCATE_DISTANCE))
+        self.updateUI()
+    }
+    
+    @IBAction func readStopButton(_ sender: UIButton) {
+        Ugi.singleton().activeInventory.stop {
+            //self.updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
